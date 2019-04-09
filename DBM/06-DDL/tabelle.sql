f58ef8b7a3fc4d728c4c7bee6a80377381f0c301
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2019-04-09 21:08:57 +0200

    SQL DDL

---
+++ DBM/06-DDL/tabelle.sql
@@ -0,0 +1,56 @@
+
+set sql_mode='STRICT_ALL_TABLES';
+
+-- create sample database
+drop database if exists personal;
+create database personal;
+
+-- use database
+use personal;
+
+-- create tables
+drop table if exists abteilung;
+create table abteilung (
+	id int not null auto_increment primary key,
+	kuerzel char(7) not null unique,
+	beschreibung varchar(60) not null,
+	letzte_aenderung timestamp
+);
+drop table if exists mitarbeiter;
+create table mitarbeiter (
+	id int not null auto_increment primary key,
+	abteilung_id int,
+	nachname varchar(30) not null,
+	vorname varchar(30),
+	personal_nr int unsigned not null unique,
+	geb_datum date default '1970-01-01',
+	geschlecht enum('M', 'W', 'D') default 'D',
+	titel set('Dr.', 'Prof.','Mag.'),
+	letzte_aenderung timestamp,
+	foreign key (abteilung_id) references abteilung(id)
+);
+drop table if exists adresse;
+create table adresse (
+	id int not null auto_increment primary key,
+	strasse varchar(60) not null,
+	plz smallint unsigned not null,
+	ort varchar(30) not null,
+	letzte_aenderung timestamp
+);
+drop table if exists mitarbeiter_adresse;
+create table mitarbeiter_adresse (
+	mitarbeiter_id int,
+	adresse_id int,
+	type enum('Hauptwohnsitz', 'Zweitwohnsitz') not null,
+	letzte_aenderung timestamp,
+	foreign key (mitarbeiter_id) references mitarbeiter(id),
+	foreign key (adresse_id) references adresse(id)
+);
+
+-- create indices
+create index ix_abteilung on abteilung(kuerzel);
+create index ix_mitarbeiter on mitarbeiter(nachname);
+
+-- show results
+show tables;
+
--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 My personal artefacts from my studies in Witschaftsinformatik - Busines Information Systems
 
-- In "Mitschriften" you'll find my notes taken in class for various subjects in Markdown format
+- in "Mitschriften" you'll find my notes taken in class for various subjects in Markdown format
 - in "PP" you'll find sample C code for the Prozedurales Programmieren class, lovingly handcrafted in vi and gcc
 - in "OPP" you'll find sample Java code for the Objetkorientiertes Programmieren class, lovingly handcrafted in vi and javac
+- in "DBM" you'll find sample SQL statemenst from the Datenbank Management class, lovingly handcrafted in vi and mysql
 
 
 Enjoy!
