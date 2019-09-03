020a96c83e60b9d86981838da11c3e2c919bc6c7
Christian Frank <42704306+chfrank-cgn@users.noreply.github.com>
2019-09-03 21:39:34 +0200

    Wintersemester

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ My personal artefacts from my studies in Witschaftsinformatik - Busines Informat
 - in "OPP" you'll find sample Java code for the Objetkorientiertes Programmieren class, lovingly handcrafted in vi and javac
 - in "DBM" you'll find sample SQL statements from the Datenbank Management class, lovingly handcrafted in vi and mysql
 - in "QMWI" you'll find sample C code for the Quantitative Methoden class, lovingly handcrafted in vi and gcc
+- in "SOP" you'll find sample Python code for the Skriptsprachen Programmieren class, lovingly handcrafted in vi and python
 
 Enjoy!
 
---
+++ SOP/00/uebung.py
@@ -0,0 +1,50 @@
+# Uebungsblatt 00
+
+# Double arithmetics
+pi = 3.14
+e = 2.78
+print
+print('Float')
+print(pi,'/',e,'=',pi / e)
+print(pi,'//',e,'=',pi // e)
+print(pi,'%',e,'=',pi % e)
+print(pi,'**',e,'=',pi ** e)
+# print(pi,'&',e,'=',pi & e)
+# print(pi,'^',e,'=',pi ^ e)
+# print(pi,'|',e,'=',pi | e)
+
+# Integer arithmetics
+pi = 32
+e = 5
+print
+print('Integer')
+print(pi,'/',e,'=',pi / e)
+print(pi,'//',e,'=',pi // e)
+print(pi,'%',e,'=',pi % e)
+print(pi,'*',e,'=',pi * e)
+print(pi,'**',e,'=',pi ** e)
+
+# Bit operations
+pi = 9
+e = 1
+print
+print('Bit')
+print(pi,'>>',e,'=',pi >> e)
+print(pi,'<<',e,'=',pi << e)
+print(pi,'&',e,'=',pi & e)
+print(pi,'|',e,'=',pi | e)
+print(pi,'^',e,'=',pi ^ e)
+
+# Boolean
+pi=11
+e=3
+print
+print('Boolean')
+print(pi,'>',e,'=',pi > e)
+print(pi,'<',e,'=',pi < e)
+print(pi,'==',e,'=',pi == e)
+print(pi,'!=',e,'=',pi != e)
+
+print
+exit()
+
